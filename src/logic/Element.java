89ed3c74fefb55659b73c311e94bb7fb91a0c607
lazarocosta <lazaroosta@hotmail.com>
2017-03-31 15:54:51 +0100

    init parser

---
+++ src/logic/Element.java
@@ -0,0 +1,26 @@
+
+package logic;
+
+public class Element {
+
+    private String element1;
+    private String field1;
+    private String characteristic1;
+
+    private String element2;
+    private String field2;
+    private String characteristic2;
+
+    public Element(String el1, String field1, String charact1, String el2, String field2, String charact2) {
+
+        this.element1 = el1;
+        this.field1 = field1;
+        this.characteristic1 = charact1;
+
+        this.element2 = el2;
+        this.field2 = field2;
+        this.characteristic2 = charact2;
+    }
+
+
+}
