6b5f3e87de36de82f20d3b2958d84de3158d54b0
lazarocosta <lazaroosta@hotmail.com>
2017-04-07 11:25:49 +0100

    add files

--- src/Autoanalyse.java
+++ src/Autoanalyse.java
@@ -46,7 +46,8 @@ public class Autoanalyse {
 
         this.state();
         this.nodesGraph();
-        this.complement();
+
+        this.outPutResult(this.complement());
     }
 
     public static void main(String args[]) {
@@ -115,35 +116,35 @@ public class Autoanalyse {
 
         return result;
     }
-    
-    public void outPutResult(ArrayList<ElementGraph> result){
-    	Path f = Paths.get("output.dot");
-    	List<String> lines = new ArrayList<String>(); 
-    	
-    	lines.add("digraph OUT {");
-    	for(ElementGraph node: result){
-    		String line = new String(node.getElement1());
-    		if(node.getElement2() == null){
-    			if(node.getField1() != null && node.getCharacteristic1() != null){
-    				line +=" [ "+ node.getField1() + "="+ node.getCharacteristic1()+ " ];";
-    			}
-    			lines.add(line);
-    		}else{
-    			line += " -> " + node.getElement2();
-    			if(node.getField2() != null && node.getCharacteristic2() != null){
-    				line +=" [ "+ node.getField2() + "="+ node.getCharacteristic2()+ " ];";
-    			}
-    			lines.add(line);
-    		}
-    	}
-    	lines.add("}");
-    	
-    	try {
-			Files.write(f,lines,Charset.forName("UTF-8"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
+
+    public void outPutResult(ArrayList<ElementGraph> result) {
+        Path f = Paths.get("output.dot");
+        List<String> lines = new ArrayList<String>();
+
+        lines.add("digraph OUT {");
+        for (ElementGraph node : result) {
+            String line = new String(node.getElement1());
+            if (node.getElement2() == null) {
+                if (node.getField1() != null && node.getCharacteristic1() != null) {
+                    line += " [" + node.getField1() + "=" + node.getCharacteristic1() + "];";
+                }
+                lines.add(line);
+            } else {
+                line += " -> " + node.getElement2();
+                if (node.getField2() != null && node.getCharacteristic2() != null) {
+                    line += " [" + node.getField2() + "=" + node.getCharacteristic2() + "];";
+                }
+                lines.add(line);
+            }
+        }
+        lines.add("}");
+
+        try {
+            Files.write(f, lines, Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
 
---
+++ src/logic/DotConvert.java
@@ -0,0 +1,53 @@
+package logic;
+
+import java.io.File;
+
+import Gui.GraphViz;
+import Gui.*;
+
+public class DotConvert 
+{
+	public static void main(String[] args)
+	{
+		DotConvert dc = new DotConvert();
+		dc.start();
+	}
+	
+	/**
+	 * Read the DOT source from a file,
+	 * convert to image and store the image in the file system.
+	 */
+	private void start()
+	{
+		//String dir = "/home/jabba/Dropbox/git.projects/laszlo.own/graphviz-java-api";     // Linux
+		//String input = dir + "/sample/simple.dot";
+		//String input = "c:/Users/Maria/workspace/COMP_proj/src/dot/example2.dot";    // Windows
+		String input = "C:/Users/Acer-PC/workspace/GitHub/COMP-Autoanalyze/dot/example2.dot";
+
+		GraphViz gv = new GraphViz();
+		gv.readSource(input);
+		System.out.println(gv.getDotSource());
+
+		String type = "png";
+		//    String type = "dot";
+		//    String type = "fig";    // open with xfig
+		//    String type = "pdf";
+		//    String type = "ps";
+		//    String type = "svg";    // open with inkscape
+		//    String type = "png";
+		//      String type = "plain";
+		
+		
+		String representationType= "dot";
+		//		String representationType= "neato";
+		//		String representationType= "fdp";
+		//		String representationType= "sfdp";
+		// 		String representationType= "twopi";
+		//		String representationType= "circo";
+		
+		//File out = new File("/tmp/simple." + type);   // Linux
+		//File out = new File("c:/Users/Maria/Documents/temp/graph2." + type);   // Windows
+		File out = new File("C:/Users/Acer-PC/workspace/GitHub/COMP-Autoanalyze/dot/graph2." + type);
+		gv.writeGraphToFile( gv.getGraph(gv.getDotSource(), type, representationType), out );
+	}
+}
