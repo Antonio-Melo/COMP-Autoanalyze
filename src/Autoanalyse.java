ce81fe259f0bf868e4195ef5a3a42b4d6d072105
lazarocosta <lazaroosta@hotmail.com>
2017-04-27 15:50:04 +0100

    add files

--- src/Autoanalyse.java
+++ src/Autoanalyse.java
@@ -1,60 +1,46 @@
-package src;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
+
+import logic.Operations.Complement;
+import logic.Parser.ValidateFile;
+import logic.Structure.Graph;
 
 
 public class Autoanalyse {
 
     private String path;
     private String operation;
-    private ArrayList<ElementGraph> tokens;
-    private ArrayList<String> nodes;
-    private ArrayList<String> stateEndNode;
+    private Graph graph;
 
     public Autoanalyse(String args[]) {
 
 
-
-     /*   if (args.length != 2) {
+        if (args.length != 2) {
             System.out.println("Requerires <path> <opeartion>");
         }
         this.path = args[0];
         this.operation = args[1];
-        */
 
-        tokens = new ArrayList<>();
-        stateEndNode = new ArrayList<>();
-        nodes = new ArrayList<>();
+        graph = new Graph();
 
 
         try {
             ValidateFile validateFile = new ValidateFile(System.in);
-            validateFile.Start(tokens);
+            validateFile.Start(graph);
 
         } catch (Throwable e) {
             System.out.println("Invalid REGEX!\n" + e.getMessage());
             System.exit(1);
         }
 
-        this.state();
-        this.nodesGraph();
+        Graph graphResult = Complement.complement(graph);
 
-        this.outPutResult(this.complement());
+       // this.outPutResult(this.complement());
     }
 
     public static void main(String args[]) {
 
         Autoanalyse auto = new Autoanalyse(args);
     }
-
+/*
     public void state() {
         // System.out.println("tokens size" + tokens.size());
 
@@ -64,12 +50,13 @@ public class Autoanalyse {
                     stateEndNode.add(elem.getElement1());
         }
 
-    /*    for (String node : stateEndNode) {
-            System.out.println(node);
-        }*/
+        //for (String node : stateEndNode) {
+            //System.out.println(node);
+        //}
         this.nodesGraph();
     }
-
+    */
+/*
     public void nodesGraph() {
 
         for (ElementGraph elem : tokens) {
@@ -83,13 +70,14 @@ public class Autoanalyse {
                     nodes.add(elem.getElement2());
                 }
         }
-       /* for (String node : nodes) {
-            System.out.println("nodes Graph-> " + node);
-        }
-        */
+       // for (String node : nodes) {
+         //   System.out.println("nodes Graph-> " + node);
+        //}
+
     }
+    */
 
-
+/*
     public ArrayList<ElementGraph> complement() {
 
         ArrayList<ElementGraph> result = new ArrayList<ElementGraph>();
@@ -116,7 +104,8 @@ public class Autoanalyse {
 
         return result;
     }
-
+    */
+/*
     public void outPutResult(ArrayList<ElementGraph> result) {
         Path f = Paths.get("output.dot");
         List<String> lines = new ArrayList<String>();
@@ -145,8 +134,9 @@ public class Autoanalyse {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-    }
-}
+            */
+   }
+
 
 
 /* #### NAO APAGAR  ###################################
