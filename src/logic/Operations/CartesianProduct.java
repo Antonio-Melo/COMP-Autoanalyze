92a49b1ff526219eec691a67ee4e0bb01d76a0ed
Antonio-Melo <happyd3nt@gmail.com>
2017-05-03 18:15:24 +0100

    starting cartesina product

--- src/logic/Operations/CartesianProduct.java
+++ src/logic/Operations/CartesianProduct.java
@@ -1,4 +1,21 @@
 package logic.Operations;
 
+import logic.Structure.Graph;
+import logic.Structure.Node;
+
 public class CartesianProduct {
+
+    public  CartesianProduct(Graph graph1, Graph graph2){
+        Graph newGraph = new Graph();
+
+        //Create Nodes
+        for(Node nodeg1 : graph1.getNodes()){
+            for(Node nodeg2 : graph2.getNodes()){
+                newGraph.addNode(new Node(new String(nodeg1.getName()+nodeg2.getName())));
+            }
+        }
+
+        //Create Edges
+        
+    }
 }
