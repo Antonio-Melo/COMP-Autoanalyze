5cee35b8e943df78db311de76dfbdc2ec63999d4
Koreris <mariadsda@gmail.com>
2017-06-05 14:29:11 +0100

    started language analysis class

---
+++ src/logic/Operations/LanguageAnalysis.java
@@ -0,0 +1,21 @@
+package logic.Operations;
+
+import logic.Structure.Graph;
+import logic.Structure.Node;
+import logic.Structure.Edge;
+
+import java.util.ArrayList;
+
+public class LanguageAnalysis 
+{
+	public static Graph getRegularExpression(Graph graph) {
+        String regExp = "";
+
+        for (Edge edge : graph.getEdges()) {
+            Node dest = edge.getDest();
+            Node src = edge.getSource();
+
+            edge.setDest(src);
+            edge.setSource(dest);
+        }
+}
